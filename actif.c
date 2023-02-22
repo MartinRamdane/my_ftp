0a162c9bde9c587a23f0106bb80853a24253ed4c
Ramdane Martin <martin.ramdane@epitech.eu>
2023-02-22 14:12:39 +0100

    fix a warning

--- actif.c
+++ actif.c
@@ -7,25 +7,6 @@
 
 #include "server.h"
 
-// void pasv_reply_code(clients_t *client, int port, char *ip)
-// {
-//     int port1 = port / 256, port2 = port % 256;
-//     write(client->ctrl_sock, "227 Entering Passive Mode (", 27);
-//     char *token = strtok(ip, ".");
-//     while (token != NULL) {
-//         write(client->ctrl_sock, token, strlen(token));
-//         write(client->ctrl_sock, ",", 1);
-//         token = strtok(NULL, ".");
-//     }
-//     char int_str[5];
-//     sprintf(int_str, "%d", port1);
-//     write(client->ctrl_sock, int_str, strlen(int_str));
-//     write(client->ctrl_sock, ",", 1);
-//     sprintf(int_str, "%d", port2);
-//     write(client->ctrl_sock, int_str, strlen(int_str));
-//     write(client->ctrl_sock, ")\r\n", 3);
-// }
-
 void create_sock(clients_t **client, char *ip, char *port)
 {
     (*client)->data_sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -45,8 +26,10 @@ char *get_port(clients_t **client, char *copy)
         port++;
     else {
         write((*client)->ctrl_sock, "501 Syntax error in parameters ", 31);
-        write((*client)->ctrl_sock, "or argument.\r\n", 14); return;
+        write((*client)->ctrl_sock, "or argument.\r\n", 14);
+        return NULL;
     }
+    return port;
 }
 
 void port_command(clients_t **client, char *line)
