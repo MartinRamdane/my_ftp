e34803ef7bff13ad3ff284f9ec7f7a553460d79b
Ramdane Martin <martin.ramdane@epitech.eu>
2023-03-07 10:23:18 +0100

    fix some errors

--- list.c
+++ list.c
@@ -7,21 +7,35 @@
 
 #include "server.h"
 
-void list_in_dir(clients_t **client, char *path)
+void print_msg(clients_t **client)
+{
+    write((*client)->ctrl_sock, "150 File status okay;", 21);
+    write((*client)->ctrl_sock, " about to open data connection.\r\n", 33);
+    set_data_sock(client);
+}
+
+int list_in_dir(clients_t **client, char *path)
 {
     DIR *fd;
     struct dirent *dir;
     fd = opendir(path);
     if (fd) {
+        print_msg(client);
+        if ((*client)->data_sock == 0) {
+            write((*client)->ctrl_sock, "425 Can't open data connection.\r\n"
+            , 33); return 1;
+        }
         while ((dir = readdir(fd)) != NULL) {
             write((*client)->data_sock, dir->d_name, strlen(dir->d_name));
             write((*client)->data_sock, "\r\n", 2);
         }
         closedir(fd);
     } else {
-        write((*client)->ctrl_sock, "501 Syntax error in parameters ", 31);
-        write((*client)->ctrl_sock, "or argument.\r\n", 14);
+        write((*client)->ctrl_sock, "450 Requested file action not ", 30);
+        write((*client)->ctrl_sock, "taken.\r\n", 8);
+        return 1;
     }
+    return 0;
 }
 
 void close_co(clients_t **client)
@@ -36,18 +50,12 @@ int check_co(clients_t **client)
         write((*client)->ctrl_sock, "530 Not logged in.\r\n", 20);
         return 1;
     }
-    write((*client)->ctrl_sock, "150 File status okay;", 21);
-    write((*client)->ctrl_sock, " about to open data connection.\r\n", 33);
-    set_data_sock(client);
-    if ((*client)->data_sock == 0) {
-        write((*client)->ctrl_sock, "425 Can't open data connection.\r\n", 33);
-        return 1;
-    }
     return 0;
 }
 
 void list_command(clients_t **client, char *line)
 {
+    int res = 0;
     if (check_co(client) == 1)
         return;
     if (strcmp(line, "LIST") == 0) {
@@ -60,7 +68,8 @@ void list_command(clients_t **client, char *line)
             write((*client)->ctrl_sock, "501 Syntax error in parameters ", 31);
             write((*client)->ctrl_sock, "or argument.\r\n", 14); return;
         }
-        list_in_dir(client, dir);
+        res = list_in_dir(client, dir);
     }
-    close_co(client);
+    if (res == 0)
+        close_co(client);
 }
